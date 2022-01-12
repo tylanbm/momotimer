2e9758aa3ea00e0b629705318aeeaf2f73bff9b4
tylanm <tylanmcpherson@gmail.com>
2022-01-12 15:02:24 -0800

    Implemented protoype timer

--- App.js
+++ App.js
@@ -1,13 +1,43 @@
+import { useState, useEffect} from 'react'
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import OptionButton from './Components/OptionButton';
+
+
+
+const workTime = 25;
+const smallBreakTime = 5;
+const largeBreakTime = 15;
+
 
 export default function App() {
+
+  const [timer, setTimer] = useState(workTime);
+
+  function Timer() {
+    useEffect(() => {
+       counter = workTime;
+
+      let oneSecInterval = setInterval(() => {
+        console.log(counter +'sec.');
+        counter--;
+        setTimer(counter);
+        if (counter == 0) {
+          clearInterval(oneSecInterval);
+        }
+      }, 1000);
+    }, []);
+  }
+
+  Timer();
+
   return (
     <View style={styles.container}>
-      <Text>OPTIONS</Text>
+      <OptionButton />
       <Text>Momo Timer</Text>
       <Text>Work like no one before.</Text>
-      <Text>25:00</Text>
+      <Text>{timer}</Text>
+      
       <Text>Dots</Text>
       <Text>Start</Text>
       <Text>Reset</Text>
---
+++ Components/OptionButton.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import { StyleSheet, Text, View, TouchableOpacity } from 'react-native'
+
+const OptionButton = () => {
+    return (
+        <View>
+            <TouchableOpacity>
+                <Text>Options</Text>
+            </TouchableOpacity>
+        </View>
+    )
+}
+
+export default OptionButton
+
+const styles = StyleSheet.create({})
