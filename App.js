9fed5cf0239964210e7765687d175f7503a1f3ce
tylanm <tylanmcpherson@gmail.com>
2022-01-12 15:56:30 -0800

    Prototyped stylesheets for main components

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import { useState, useEffect} from 'react'
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
 import OptionButton from './Components/OptionButton';
 
 
@@ -22,25 +22,34 @@ export default function App() {
         console.log(counter +'sec.');
         counter--;
         setTimer(counter);
-        if (counter == 0) {
+        if (counter < 0) {
           clearInterval(oneSecInterval);
         }
       }, 1000);
     }, []);
   }
 
-  Timer();
+  //Timer();
 
   return (
     <View style={styles.container}>
       <OptionButton />
-      <Text>Momo Timer</Text>
-      <Text>Work like no one before.</Text>
-      <Text>{timer}</Text>
+      <Text style={styles.title}>Momo Timer</Text>
+      <Text style={styles.subtitle}>Work like no one has before.</Text>
+      <Text style={styles.timerDisplay}>{timer}:00</Text>
       
       <Text>Dots</Text>
-      <Text>Start</Text>
-      <Text>Reset</Text>
+
+      <View style={styles.buttonContainer}>
+        <TouchableOpacity style={styles.startButton}>
+          <Text style={styles.startButtonText}>Start</Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity style={styles.resetButton}>
+          <Text style={styles.resetButtonText}>Reset</Text>
+        </TouchableOpacity>
+      </View>
+
       <StatusBar style="auto" />
     </View>
   );
@@ -52,5 +61,47 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  title: {
+    fontFamily: "WorkSans",
+    fontSize: 54,
+    fontWeight: '700', 
+  },
+  subtitle: {
+    fontFamily: "WorkSans",
+    fontSize: 20,
+    fontWeight: '700',
+  },
+  timerDisplay: {
+    fontFamily: "WorkSans",
+    fontSize: 84,
+    fontWeight: 'normal',
+  },
+  buttonContainer: {
+    flexDirection: 'row',
+  },
+  startButton: {
+    width: 100,
+    paddingVertical: 10,
+    borderRadius:11,
+    backgroundColor: '#0071E3',
+    alignItems: 'center',
+  },
+  startButtonText: {
+    color: 'white',
+    fontSize: 14,
+    fontFamily: 'WorkSans',
+  },
+  resetButton: {
+    width: 100,
+    paddingVertical: 10,
+    borderRadius: 11,
+    backgroundColor: '#EBEBEB',
+    alignItems: 'center',
+  },
+  resetButtonText: {
+    color: '#272729',
+    fontSize: 14,
+    fontFamily: 'WorkSans',
   },
 });
